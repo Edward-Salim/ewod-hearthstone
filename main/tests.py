1466061f874ce751fe638022cbd1a5910dd5042c
Edward-Salim <edwardsalim29@gmail.com>
2023-09-07 01:17:13 +0700

    Membuat README.md dan Menyelesaikan Tugas 2

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from .models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,8 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_item_model(self):
+        item = Item.objects.create(name='Test Item', amount=10, description='Test Description')
+        retrieved_item = Item.objects.get(pk=item.pk)
+        self.assertEqual(item, retrieved_item)
