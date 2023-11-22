a8452b1915bbc84874765c22d94b1466e689d75c
Edward-Salim <edwardsalim29@gmail.com>
2023-11-22 13:58:44 +0700

    Menyelesaikan Tutorial 9

--- ewod_hearthstone/settings.py
+++ ewod_hearthstone/settings.py
@@ -66,6 +66,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://edward-salim-tugas.pbp.cs.ui.ac.id"]
 
 ROOT_URLCONF = 'ewod_hearthstone.urls'
 
